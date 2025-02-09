02f4eff75e21eaa84d22d27a051e3fa7b57b4d72
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-02-08 20:47:21 -0500

    docs(api): add documentation in AppConfiguration extension class

--- src/Openlysis.API/AppConfiguration.cs
+++ src/Openlysis.API/AppConfiguration.cs
@@ -2,8 +2,15 @@ using FastEndpoints;
 
 namespace Openlysis.API;
 
+/// <summary>
+/// Configuration of the API for the web application.
+/// </summary>
 public static class AppConfiguration
 {
+    /// <summary>
+    /// Configures the API for the web application.
+    /// </summary>
+    /// <param name="app">The WebApplication instance to configure.</param>
     public static void ConfigureApi(this WebApplication app)
     {
         if (app.Environment.IsDevelopment())
