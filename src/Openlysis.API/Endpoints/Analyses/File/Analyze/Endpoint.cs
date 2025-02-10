6353dfd13d044f4697cbbd426ab23c4da1f30bc0
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-02-09 19:30:54 -0500

    feat: add endpoint to upload a file to be analyzed
    - Removes unnecessary CreationDate from file information in domain and infrastructure layer.
    - AnalyzeCommand and handler returns a new response with all file analysis information.

---
+++ src/Openlysis.API/Endpoints/Analyses/File/Analyze/Endpoint.cs
@@ -0,0 +1,98 @@
+using FastEndpoints;
+
+using MediatR;
+
+using Openlysis.Application.FileAnalyses.Commands;
+
+namespace Openlysis.API.Endpoints.Analyses.File.Analyze;
+
+/// <summary>
+/// Endpoint to analyze a file.
+/// </summary>
+public class Endpoint : Endpoint<Request, Response>
+{
+    private readonly IMediator _mediator;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Endpoint"/> class.
+    /// </summary>
+    /// <param name="mediator">Mediator to send commands and receive responses to application layer.</param>
+    public Endpoint(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Configures the endpoint.
+    /// </summary>
+    public override void Configure()
+    {
+        Post(string.Empty);
+        Group<FileAnalysesGroup>();
+        AllowFileUploads();
+        Description(
+            b =>
+            {
+                b.WithName("AnalyzeFile");
+                b.WithDisplayName("AnalyzeFile");
+                b.Accepts<Request>(contentType: "multipart/form-data");
+                b.Produces<Response>(StatusCodes.Status201Created);
+                b.ProducesProblemDetails();
+            });
+        Summary(
+            s =>
+            {
+                s.Summary = "Uploads a file.";
+                s.Description = "Uploads a file to be analyzed.";
+            });
+    }
+
+    /// <summary>
+    /// Handles the file analysis request.
+    /// </summary>
+    /// <param name="request">The request containing the file to be analyzed.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        if (request.File.Length <= 0)
+        {
+            await SendResultAsync(Results.Problem(
+                statusCode: StatusCodes.Status400BadRequest,
+                detail: "Provided file has no content."));
+        }
+
+        AnalyzeFileCommand command;
+
+        await using (var memoryStream = new MemoryStream())
+        {
+            await request.File.CopyToAsync(memoryStream, ct);
+            command = new AnalyzeFileCommand(
+                request.File.FileName,
+                request.File.ContentType,
+                FileData: memoryStream.ToArray(),
+                Reanalyze: request.Reanalyze);
+        }
+
+        var fileAnalysisOrError = await _mediator.Send(command, ct);
+
+        Response = new Response(
+            fileAnalysisOrError.Value.Id.Value.ToString(),
+            fileAnalysisOrError.Value.File.HashSet.Md5,
+            fileAnalysisOrError.Value.File.HashSet.Sha1,
+            fileAnalysisOrError.Value.File.HashSet.Sha256,
+            fileAnalysisOrError.Value.File.HashSet.Sha512);
+
+        var routeValues = new Dictionary<string, string>
+        {
+            { "id", Response.FileAnalysisId },
+        };
+
+        await SendCreatedAtAsync(
+            "GetFileAnalysisById",
+            routeValues,
+            Response,
+            generateAbsoluteUrl: true,
+            ct);
+    }
+}
--- src/Openlysis.Application/FileAnalyses/Commands/AnalyzeFileCommandHandler.cs
+++ src/Openlysis.Application/FileAnalyses/Commands/AnalyzeFileCommandHandler.cs
@@ -1,7 +1,5 @@
 using ErrorOr;
 
-using FileSignatures;
-
 using MediatR;
 
 using Openlysis.Application.Common.Interfaces.Persistence;
@@ -19,7 +17,7 @@ namespace Openlysis.Application.FileAnalyses.Commands;
 /// <summary>
 /// Handles <see cref="AnalyzeFileCommand"/>.
 /// </summary>
-public class AnalyzeFileCommandHandler : IRequestHandler<AnalyzeFileCommand, ErrorOr<FileAnalysisId>>
+public class AnalyzeFileCommandHandler : IRequestHandler<AnalyzeFileCommand, ErrorOr<FileAnalysis>>
 {
     private readonly IFileAnalysisRepository _fileAnalysisRepository;
     private readonly IEnumerable<IFileAnalyzer> _fileAnalyzers;
@@ -46,7 +44,7 @@ public class AnalyzeFileCommandHandler : IRequestHandler<AnalyzeFileCommand, Err
     }
 
     /// <inheritdoc/>
-    public async Task<ErrorOr<FileAnalysisId>> Handle(AnalyzeFileCommand command, CancellationToken cancellationToken)
+    public async Task<ErrorOr<FileAnalysis>> Handle(AnalyzeFileCommand command, CancellationToken cancellationToken)
     {
         HashSet hashSet;
         using (var dataMemoryStream = new MemoryStream(command.FileData))
@@ -63,30 +61,17 @@ public class AnalyzeFileCommandHandler : IRequestHandler<AnalyzeFileCommand, Err
                 await AnalyzeFileAsync(command.FileData, cancellationToken);
             }
 
-            return existingAnalysis.Id;
+            return existingAnalysis;
         }
 
         await AnalyzeFileAsync(command.FileData, cancellationToken);
 
         // Save the new file analysis in database.
-        var fileFormatInspector = new FileFormatInspector();
-        var mimeType = "other";
-
-        using (var stream = new MemoryStream(command.FileData))
-        {
-            var format = fileFormatInspector.DetermineFileFormat(stream);
-
-            if (format is not null)
-            {
-                mimeType = format.MediaType;
-            }
-        }
-
-        var fileGeneralInfo = new FileGeneralInfo(command.Filename, mimeType, command.FileData.Length, command.CreationDate);
+        var fileGeneralInfo = new FileGeneralInfo(command.FileName, command.FileContentType, command.FileData.Length);
         var fileInfo = new File(hashSet, fileGeneralInfo);
         var fileAnalysis = FileAnalysis.Create(_timeProvider.GetUtcNow().DateTime, Verdict.Undetected, fileInfo, []);
         await _fileAnalysisRepository.AddAsync(fileAnalysis);
-        return fileAnalysis.Id;
+        return fileAnalysis;
     }
 
     private async Task AnalyzeFileAsync(byte[] fileData, CancellationToken cancellationToken)
