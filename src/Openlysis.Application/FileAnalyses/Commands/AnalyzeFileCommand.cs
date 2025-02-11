7703ce6cac2ca60568ecb771082a2bb2bc2d2eef
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-02-10 20:19:34 -0500

    docs(app): add missing documentation to AnalyzeFileCommand

--- src/Openlysis.Application/FileAnalyses/Commands/AnalyzeFileCommand.cs
+++ src/Openlysis.Application/FileAnalyses/Commands/AnalyzeFileCommand.cs
@@ -9,7 +9,11 @@ namespace Openlysis.Application.FileAnalyses.Commands;
 /// <summary>
 /// Command to analyze a file.
 /// </summary>
-/// <param name="FileData">Data of the file to be analyzed.</param>
+/// <param name="FileName">The name of the file.</param>
+/// <param name="FileContentType">The content type of the file.</param>
+/// <param name="FileData">The data stream of the file.</param>
+/// <param name="Reanalyze">Indicates whether to reanalyze the file, if another user
+/// has already analyzed.</param>
 public record AnalyzeFileCommand(
     string FileName,
     string FileContentType,
