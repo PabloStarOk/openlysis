1991c30d2c9e0833daafe64de0de3e37fced4490
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-05-01 23:47:40 -0500

    build(docker): move migration script creation logic to db dockerfile

--- docker/db/scripts/init.sh
+++ docker/db/scripts/init.sh
@@ -3,15 +3,28 @@
 SCRIPTS_DIRPATH=/scripts
 AUTH_SCRIPT=$SCRIPTS_DIRPATH/00_auth.sql
 DB_SCRIPT=$SCRIPTS_DIRPATH/01_db.sql
+MIGRATIONS_SCRIPT=$SCRIPTS_DIRPATH/02_migrations.sql
+TABLES_SCRIPT=$SCRIPTS_DIRPATH/03_tables.sql
 
 # Create API user
 psql -d "postgres" \
     -U "postgres" \
+    -v API_DB_USER_NAME="$API_DB_USER_NAME" \
     -v API_USER_DB_PASSWORD="$API_USER_DB_PASSWORD" \
     -f "$AUTH_SCRIPT"
 
 # Create API DB
 psql -d "postgres" \
-    -U "$API_USER" \
+    -U "$API_DB_USER_NAME" \
     -v API_DB_NAME="$API_DB_NAME" \
     -f "$DB_SCRIPT"
+
+# Run migrations script
+psql -d "$API_DB_NAME" \
+    -U "$API_DB_USER_NAME" \
+    -f "$MIGRATIONS_SCRIPT"
+
+# Create API tables
+psql -d "$API_DB_NAME" \
+    -U "$API_DB_USER_NAME" \
+    -f "$TABLES_SCRIPT"
