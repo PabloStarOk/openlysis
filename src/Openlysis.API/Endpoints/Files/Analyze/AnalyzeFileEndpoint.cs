08c95d2d181381205e69dea0a3acf051f5c0c372
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-06-09 14:27:22 -0500

    refactor(api): simplify request's property name removing file prefix from 'password'

--- src/Openlysis.API/Endpoints/Files/Analyze/AnalyzeFileEndpoint.cs
+++ src/Openlysis.API/Endpoints/Files/Analyze/AnalyzeFileEndpoint.cs
@@ -58,7 +58,7 @@ public class AnalyzeFileEndpoint : Endpoint<AnalyzeFileRequest, AnalyzeFileRespo
                 s.Summary = "Uploads a file.";
                 s.Description = "Uploads a file to be analyzed.";
                 s.RequestParam(r => r.File, "File to be analyzed.");
-                s.RequestParam(r => r.FilePassword, "Password of the file if it is protected (Not recommended to upload confidential files) (Optional).");
+                s.RequestParam(r => r.Password, "Password of the file if it is protected (Not recommended to upload confidential files) (Optional).");
                 s.RequestParam(r => r.IsPrivate, "If the file analysis is private. True is the default. (Optional)");
                 s.RequestParam(r => r.Reanalyze, "If the file must analyzed again, instead of returning the last analysis. False is the default. (Optional).");
             });
@@ -86,7 +86,7 @@ public class AnalyzeFileEndpoint : Endpoint<AnalyzeFileRequest, AnalyzeFileRespo
         var fileData = new FileData(
             request.File.FileName,
             request.File.ContentType,
-            request.FilePassword,
+            request.Password,
             stream);
         var result = await _multiAnalysisService.AnalyzeAsync(
             userId,
--- src/Openlysis.API/Endpoints/Files/Analyze/AnalyzeFileRequest.cs
+++ src/Openlysis.API/Endpoints/Files/Analyze/AnalyzeFileRequest.cs
@@ -4,11 +4,11 @@ namespace Openlysis.API.Endpoints.Files.Analyze;
 /// Request to analyze a file.
 /// </summary>
 /// <param name="File">File to analyze</param>
-/// <param name="FilePassword">Password for the file if it is protected</param>
+/// <param name="Password">Password for the file if it is protected</param>
 /// <param name="IsPrivate">Indicates if the file multi analysis should be private.</param>
 /// <param name="Reanalyze">If file has already been analyzed by another user, reanalyze it again.</param>
 public record AnalyzeFileRequest(
     IFormFile? File,
-    string FilePassword = "",
+    string Password = "",
     bool IsPrivate = true,
     bool Reanalyze = false);
