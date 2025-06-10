45a56c0fcdea9638f342c15135e6c25df06f18db
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-06-10 17:56:22 -0500

    feat(api): add missing id for phone reputations and standardize other dtos

--- src/Openlysis.API/Endpoints/EmailAddresses/GetReputation/EmailAddressMultiReputationDto.cs
+++ src/Openlysis.API/Endpoints/EmailAddresses/GetReputation/EmailAddressMultiReputationDto.cs
@@ -6,19 +6,19 @@ namespace Openlysis.API.Endpoints.EmailAddresses.GetReputation;
 /// <summary>
 /// Data transfer object for <see cref="EmailAddressMultiReputation"/>.
 /// </summary>
-/// <param name="Id">The unique identifier of the email address.</param>
-/// <param name="ReputationEvaluationDate">The date when the reputation was evaluated.</param>
+/// <param name="Id">The unique identifier of the multi-reputation.</param>
+/// <param name="EvaluationDate">The date when the reputation was evaluated.</param>
 /// <param name="FinalVerdict">The final verdict assigned to the email address based on multiple reputations.</param>
 /// <param name="FinalThreatZone">The final threat zone classification for the email address.</param>
-/// <param name="EmailAddress">The email address being evaluated.</param>
-/// <param name="Reputations">An array of reputation details from different sources or services.</param>
+/// <param name="EmailAddress">The email address associated with the multi-reputation.</param>
+/// <param name="Reputations">A collection of <see cref="EmailAddressReputationDto"/>.</param>
 public record EmailAddressMultiReputationDto(
     string Id,
-    DateTime ReputationEvaluationDate,
+    DateTime EvaluationDate,
     Verdict FinalVerdict,
     ThreatZone FinalThreatZone,
     string EmailAddress,
-    EmailAddressReputationDto[] Reputations)
+    IEnumerable<EmailAddressReputationDto> Reputations)
 {
     /// <summary>
     /// Converts an <see cref="EmailAddressMultiReputation"/> instance to an <see cref="EmailAddressMultiReputationDto"/>.
@@ -28,16 +28,12 @@ public record EmailAddressMultiReputationDto(
     public static EmailAddressMultiReputationDto Parse(
         EmailAddressMultiReputation source)
     {
-        EmailAddressReputationDto[] reputations = source.Reputations
-            .Select(EmailAddressReputationDto.Parse)
-            .ToArray();
-
         return new EmailAddressMultiReputationDto(
             source.Id.Value.ToString(),
             source.ReputationEvaluationDate,
             source.FinalVerdict,
             source.FinalThreatZone,
             source.EmailAddress.Address,
-            reputations);
+            source.Reputations.Select(EmailAddressReputationDto.Parse));
     }
 }
--- src/Openlysis.API/Endpoints/Phones/GetReputation/PhoneMultiReputationDto.cs
+++ src/Openlysis.API/Endpoints/Phones/GetReputation/PhoneMultiReputationDto.cs
@@ -6,17 +6,19 @@ namespace Openlysis.API.Endpoints.Phones.GetReputation;
 /// <summary>
 /// Data transfer object for <see cref="PhoneMultiReputation"/>.
 /// </summary>
-/// <param name="EvaluationDate">The date when the assessment was performed.</param>
-/// <param name="FinalVerdict">The final verdict of the phone number's reputation.</param>
-/// <param name="FinalThreatZone">The final threat zone associated with the phone number.</param>
-/// <param name="PhoneNumber">The phone number associated with the reputation.</param>
-/// <param name="Reputations">An array of reputation details from different sources or services.</param>
+/// <param name="Id">The unique identifier of the multi-reputation.</param>
+/// <param name="EvaluationDate">The date when the reputation was evaluated.</param>
+/// <param name="FinalVerdict">The final verdict assigned to the phone number based on multiple reputations.</param>
+/// <param name="FinalThreatZone">The final threat zone classification for the phone number.</param>
+/// <param name="PhoneNumber">The phone number associated with the multi-reputation.</param>
+/// <param name="Reputations">A collection of <see cref="PhoneReputationDto"/>.</param>
 public record PhoneMultiReputationDto(
+    string Id,
     DateTime EvaluationDate,
     Verdict FinalVerdict,
     ThreatZone FinalThreatZone,
     string PhoneNumber,
-    PhoneReputationDto[] Reputations)
+    IEnumerable<PhoneReputationDto> Reputations)
 {
     /// <summary>
     /// Parses a <see cref="PhoneMultiReputation"/> object into a <see cref="PhoneMultiReputationDto"/>.
@@ -26,10 +28,11 @@ public record PhoneMultiReputationDto(
     public static PhoneMultiReputationDto Parse(PhoneMultiReputation reputation)
     {
         return new PhoneMultiReputationDto(
+            reputation.Id.Value.ToString(),
             reputation.ReputationEvaluationDate,
             reputation.FinalVerdict,
             reputation.FinalThreatZone,
             reputation.PhoneNumber,
-            reputation.Reputations.Select(PhoneReputationDto.Parse).ToArray());
+            reputation.Reputations.Select(PhoneReputationDto.Parse));
     }
 }
