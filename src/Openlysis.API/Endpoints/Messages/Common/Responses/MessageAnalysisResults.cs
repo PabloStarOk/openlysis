bd920eddecf6650e2249257c22d05eec67487050
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-06-10 17:16:59 -0500

    refactor(api): rename message analysis results properties for better standardization

--- src/Openlysis.API/Endpoints/Messages/Common/Responses/MessageAnalysisResults.cs
+++ src/Openlysis.API/Endpoints/Messages/Common/Responses/MessageAnalysisResults.cs
@@ -8,12 +8,12 @@ namespace Openlysis.API.Endpoints.Messages.Common.Responses;
 /// <summary>
 /// Analysis and reputation results of the detected data in a message.
 /// </summary>
-/// <param name="FileMultiAnalyses">A collection of file multi-analysis results.</param>
-/// <param name="UrlMultiAnalyses">A collection of URL multi-analysis results.</param>
-/// <param name="EmailAddressesReputations">A collection of email address reputation results.</param>
-/// <param name="PhoneNumbersReputations">A collection of phone number reputation results.</param>
+/// <param name="FileMultiAnalyses">A collection of <see cref="FileMultiAnalysisDto"/>.</param>
+/// <param name="UrlMultiAnalyses">A collection of <see cref="UrlMultiAnalysisDto"/>.</param>
+/// <param name="EmailAddressMultiReputations">A collection of <see cref="EmailAddressMultiReputationDto"/>.</param>
+/// <param name="PhoneNumberMultiReputations">A collection of <see cref="PhoneMultiReputationDto"/>.</param>
 public record MessageAnalysisResults(
     IEnumerable<FileMultiAnalysisDto> FileMultiAnalyses,
     IEnumerable<UrlMultiAnalysisDto> UrlMultiAnalyses,
-    IEnumerable<EmailAddressMultiReputationDto> EmailAddressesReputations,
-    IEnumerable<PhoneMultiReputationDto> PhoneNumbersReputations);
+    IEnumerable<EmailAddressMultiReputationDto> EmailAddressMultiReputations,
+    IEnumerable<PhoneMultiReputationDto> PhoneNumberMultiReputations);
