590192ade509096d2f86a7f17a1eae61fc30a6ad
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-06-14 22:10:47 -0500

    feat(api): add id to phone reputation dto for better standardization

--- src/Openlysis.API/Endpoints/Phones/GetReputation/PhoneReputationDto.cs
+++ src/Openlysis.API/Endpoints/Phones/GetReputation/PhoneReputationDto.cs
@@ -7,11 +7,13 @@ namespace Openlysis.API.Endpoints.Phones.GetReputation;
 /// <summary>
 /// Data transfer object for <see cref="PhoneReputation"/>.
 /// </summary>
+/// <param name="Id">The unique identifier of the reputation.</param>
 /// <param name="ServiceName">The name of the service that returned the multi-reputation.</param>
 /// <param name="Verdict">The verdict of the multi-reputation.</param>
 /// <param name="ThreatZone">The threat zone classification of the multi-reputation.</param>
 /// <param name="PhoneInfo">Detailed information about the phone.</param>
 public record PhoneReputationDto(
+    string Id,
     string ServiceName,
     Verdict Verdict,
     ThreatZone ThreatZone,
@@ -25,6 +27,7 @@ public record PhoneReputationDto(
     public static PhoneReputationDto Parse(PhoneReputation source)
     {
         return new PhoneReputationDto(
+            source.Id.Value.ToString(),
             source.ServiceName,
             source.Verdict,
             source.ThreatZone,
