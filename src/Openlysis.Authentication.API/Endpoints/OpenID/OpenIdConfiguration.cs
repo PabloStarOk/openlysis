3d5ba460df1609e92ad7506690a6f0fefefa8f91
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-08-12 21:56:12 -0500

    feat(auth): use snake case naming convention for OpenID config obj

--- src/Openlysis.Authentication.API/Endpoints/OpenID/OpenIdConfiguration.cs
+++ src/Openlysis.Authentication.API/Endpoints/OpenID/OpenIdConfiguration.cs
@@ -1,9 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace Openlysis.Authentication.API.Endpoints.OpenID;
 
 /// <summary>
 /// A basic configuration based on OpenID Connect for the authentication API.
 /// </summary>
-internal sealed class OpenIdConfiguration
+internal sealed record OpenIdConfiguration
 {
     /// <summary>
     /// The configuration section name for OpenID Connect settings.
@@ -13,25 +15,30 @@ internal sealed class OpenIdConfiguration
     /// <summary>
     /// Gets or sets the issuer identifier for the OpenID provider.
     /// </summary>
+    [JsonPropertyName("issuer")]
     required public string Issuer { get; set; }
 
     /// <summary>
     /// Gets or sets the endpoint URI for token requests.
     /// </summary>
+    [JsonPropertyName("token_endpoint")]
     required public Uri TokenEndpoint { get; set; }
 
     /// <summary>
     /// Gets or sets the URI for the JSON Web Key Set (JWKS) document.
     /// </summary>
+    [JsonPropertyName("jwks_uri")]
     required public Uri JwksUri { get; set; }
 
     /// <summary>
     /// Gets or sets the subject types supported by the provider.
     /// </summary>
+    [JsonPropertyName("subject_types_supported")]
     required public string[] SubjectTypesSupported { get; set; }
 
     /// <summary>
     /// Gets or sets the signing algorithms supported for ID tokens.
     /// </summary>
+    [JsonPropertyName("id_token_signing_alg_values_supported")]
     required public string[] IdTokenSigningAlgValuesSupported { get; set; }
 }
